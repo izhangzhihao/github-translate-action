4160d09bf50a76077ec2d79b007f78d3e35d5fa4
izhangzhihao <12044174+izhangzhihao@users.noreply.github.com>
2025-02-07 11:29:16 +0000

    [BOT] Update dist folder.

--- dist/index.js
+++ dist/index.js
@@ -836,7 +836,7 @@ function translate(text) {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             const modelName = (_a = process.env['MODEL_NAME']) !== null && _a !== void 0 ? _a : 'deepseek/deepseek-chat';
-            const content = `这是一个用户提交的issue/issue title，请检查原文是否包含非英文的内容，如果包含请翻译成英文，请注意，请务必保持格式不变，只返回翻译后的内容，否则展示会有问题: ${text}`;
+            const content = `这是一个用户提交的issue/issue title，请检查原文是否包含非英文的内容，如果包含请翻译成英文，请注意，请务必保持格式不变，只返回翻译后的内容，如果需要翻译的内容有特殊符号例如@@====，请务必保留，否则展示会有问题: ${text}`;
             let response = yield client.chat.completions.create({
                 messages: [{ role: 'user', content: content }],
                 model: modelName,
